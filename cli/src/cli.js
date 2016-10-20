a65cb82aece9260f810930670909e475dbef6cd3
ivmikh <ivan.mikhaylov@gmail.com>
2016-10-19 21:34:32 -0500

    Everything works, but not well enough.
    Repetition of previous command moved to Server (ClientHandler), some improvements done, like an identical user is rejected, whispering to a blank user, special symbols in messages are possible, etc. Broadcast command added, if not before.

--- cli/src/cli.js
+++ cli/src/cli.js
@@ -1,5 +1,5 @@
 import vorpal from 'vorpal'
-import { words } from 'lodash'
+// import { words } from 'lodash'
 import { connect } from 'net'
 import { Message } from './Message'
 
@@ -8,13 +8,13 @@ export const cli = vorpal()
 let username
 let server
 
-let hostDefault = 'localhost'
-let portDefault = 8080
+const hostDefault = 'localhost'
+const portDefault = 8080
 let host
 let port
-let command = ''
+
 // It should be a separate file commands.json with corresponding aliases, colors, and messages.
-let commands = [ 'users', 'u', 'echo', 'e', 'disconnect', 'd' ]
+let commands = [ 'users', 'u', 'echo', 'e', 'disconnect', 'd', 'broadcast', 'b' ]
 
 cli
   .delimiter(cli.chalk['yellow']('ftd~$'))
@@ -31,6 +31,9 @@ cli
     server = connect({ host: host, port: port }, () => {  // what if it cannot connect to a given host/port? (to be done!)
       server.write(new Message({ username, command: 'connect' }).toJSON() + '\n')
       callback()
+    })
+    server.listen(() => {
+      this.log('server bound')
     })
 
     let color = 'blue'
@@ -62,32 +65,32 @@ cli
   })
   // .delimiter(cli.chalk['green'](`${username}>`))
   .action(function (input, callback) {
-    const inputWords = words(input)
-    // this.log(`You entered: <${inputWords}>`)
-    let [ userCommand, ...rest ] = inputWords
+    // const inputWords = words(input)
+    const [ command, ...rest ] = input.split(' ')
 
-// Checking if the user provided command matches known commands:
-    if (commands.some(cmd => cmd === userCommand) || input.charAt(0) === '@') {
-      command = userCommand  // The command changes
-      // this.log(`Command <${command}> recognized`)
-    } else {                 // using the previous command
-      // this.log(`Command <${userCommand}> not recognized`)
-      rest = inputWords
-    }
+// // Checking if the user provided command matches known commands:
+//     if (commands.some(cmd => cmd === userCommand) || input.charAt(0) === '@') {
+//       command = userCommand  // The command changes
+//     } else {                 // using the previous command
+//       rest = input
+//     }
     const contents = rest.join(' ')
 
 // Checking again, which is redundant:
-    if (command === 'disconnect' || command === 'd') {                      // alias added
+    if (command === '' || command === 'help') {
+      this.log(`Possible commands and aliases: ${commands}`)
+    } else if (command === 'disconnect' || command === 'd') {                      // alias added
       server.end(new Message({ username, command: 'disconnect' }).toJSON() + '\n')
+    } else if (command === 'broadcast' || command === 'b') {                     // alias added
+      server.write(new Message({ username, command: 'broadcast', contents }).toJSON() + '\n')
     } else if (command === 'echo' || command === 'e') {                     // alias added
       server.write(new Message({ username, command: 'echo', contents }).toJSON() + '\n')
     } else if (command === 'users' || command === 'u') {                    // alias added
       server.write(new Message({ username, command: 'users', contents }).toJSON() + '\n')
-    } else if (input.charAt(0) === '@') {                     // alias added
-      server.write(new Message({ username, command: `@${command}`, contents }).toJSON() + '\n')
+    // } else if (command.charAt(0) === '@') {
+    //   server.write(new Message({ username, command, contents }).toJSON() + '\n')
     } else {
-      this.log(`Command <${command}> was not recognized \n
-        please, use one of the following: ${commands}`)
+      server.write(new Message({ username, command, contents }).toJSON() + '\n')
     }
 
     callback()
--- src/main/java/com/cooksys/assessment/server/ClientHandler.java
+++ src/main/java/com/cooksys/assessment/server/ClientHandler.java
@@ -11,9 +11,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,87 +34,152 @@ public class ClientHandler implements Runnable {
 
 	public void run() {
 		try {
-			
 
 			ObjectMapper mapper = new ObjectMapper();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 			
 			DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");  // Date format for timestamp
-//			String previousCommand = "";
+			String previousCommand = "";
 
 			while (!socket.isClosed()) {
 				String raw = reader.readLine();
 				Message message = mapper.readValue(raw, Message.class);
-														// Recording a single command pre-history:
+
 				String command = message.getCommand();
-//				command = (command == "" || command == null) ? previousCommand : command; //doesn't work yet: empty command is not passed to the server
-//				previousCommand = command;           // This is moved to Client.
-				
-				switch (command) {
+				String user = message.getUsername();
+				boolean repeatCommand = true;
+				while (repeatCommand) {
+					repeatCommand = false;
+					switch (command) {
 					case "connect":
+						// Checking out if the user is already connected:
+						if (connectedUsers.keySet().contains(user)) {
+							log.info("user <{}> is rejected because already connected", user);
+							message.setContents(df.format(new Date()) + ": <" + user + "> is already connected.");
+							String response = mapper.writeValueAsString(message);
+							writer.write(response);
+							writer.flush();
+							this.socket.close();
+							break;
+						}
+						// add user to the list:
+
+						// Write to the other users:
+						message.setContents(df.format(new Date()) + ": <" + user + "> has connected."); // ugly
+						for (String userAnother : connectedUsers.keySet()) {
+							PrintWriter writerOther = new PrintWriter(
+									connectedUsers.get(userAnother).getOutputStream());
+							writerOther.write(mapper.writeValueAsString(message));
+							writerOther.flush();
+						}
+						connectedUsers.put(user, socket);
 						log.info("user <{}> connected", message.getUsername());
-						//Write to the other users:	
-						message.setContents( df.format(new Date()) + ": <" + message.getUsername() + "> has connected." ); //ugly formatted output
-					for (String user : connectedUsers.keySet()) {
-						PrintWriter writerOther = new PrintWriter(connectedUsers.get(user).getOutputStream());
-						writerOther.write(mapper.writeValueAsString(message));
-						writerOther.flush();
-					}
-					//add user from the list:
-						connectedUsers.put(message.getUsername(), socket);
 						break;
-					case "disconnect": 
+					case "disconnect":
 						log.info("user <{}> disconnected", message.getUsername());
 						connectedUsers.remove(message.getUsername());
-						//Write to the other users:	
-						message.setContents( df.format(new Date()) + ": <" + message.getUsername() + "> has disconnected."); //ugly formatted output
-					for (String user : connectedUsers.keySet()) {
-						PrintWriter writerOther = new PrintWriter(connectedUsers.get(user).getOutputStream());
-						writerOther.write(mapper.writeValueAsString(message));
-						writerOther.flush();
-					}
+						// Write to the other users:
+						message.setContents(
+								df.format(new Date()) + ": <" + message.getUsername() + "> has disconnected."); // ugly
+																												// formatted
+																												// output
+						for (String userAnother : connectedUsers.keySet()) {
+							PrintWriter writerOther = new PrintWriter(
+									connectedUsers.get(userAnother).getOutputStream());
+							writerOther.write(mapper.writeValueAsString(message));
+							writerOther.flush();
+						}
 						this.socket.close();
 						break;
-					case "echo": 
+					case "echo":
 						log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
 						// Date dateobj = new Date();
-						message.setContents( df.format(new Date()) + " <" + message.getUsername() + "> (echo): " + message.getContents()); //ugly formatted output
+						message.setContents(df.format(new Date()) + " <" + message.getUsername() + "> (echo): "
+								+ message.getContents()); // ugly formatted
+															// output
 						String response = mapper.writeValueAsString(message);
 						writer.write(response);
 						writer.flush();
+						previousCommand = command;
 						break;
-					case "users": 
+					case "users":
 						log.info("user <{}> requested a list of currently connected users", message.getUsername());
-						message.setContents(df.format(new Date()) + ": currently connected users: \n" + 
-						connectedUsers.keySet().toString().replace("[", "").replace("]", "").replace(", ", "\n")); //that is ugly, will correct later
-						String response1 = mapper.writeValueAsString(message);
-						writer.write(response1);
+						// that is ugly formatted line (to be improved):
+						message.setContents(df.format(new Date()) + ": currently connected users: \n" + connectedUsers
+								.keySet().toString().replace("[", "").replace("]", "").replace(", ", "\n"));
+						writer.write(mapper.writeValueAsString(message));
 						writer.flush();
+						previousCommand = command;
 						break;
-//					case "":									 // in case if no commands in pre-history
-//						log.info("user <{}> missed a command", message.getUsername());
-//						message.setContents("a command is required"); 
-//						writer.write(mapper.writeValueAsString(message));
-//						writer.flush();
-//						break;
-					default:
-						if (command.charAt(0) == '@') {        // whisper:
-							String user2 = command.substring(1);
-							log.info("user <{}> whispered to user <{}> message: <{}>", message.getUsername(), user2, message.getContents());
-//							message.setCommand("echo");
-							message.setContents( df.format(new Date()) + " <" + message.getUsername() + "> (whisper): " + message.getContents()); //ugly formatted output
-							//Write to the other user:							
-							PrintWriter writerOther = new PrintWriter(connectedUsers.get(user2).getOutputStream());
+					case "broadcast":
+						log.info("user <{}> connected", message.getUsername());
+						// Write to the other users:
+						// `${timestamp} <${username}> (all): ${contents}`
+						message.setContents(df.format(new Date()) + " <" + user + "> (all): " + message.getContents()); // ugly
+						for (String userAnother : connectedUsers.keySet()) {
+							PrintWriter writerOther = new PrintWriter(
+									connectedUsers.get(userAnother).getOutputStream());
 							writerOther.write(mapper.writeValueAsString(message));
 							writerOther.flush();
+						}
+						previousCommand = command;
+						break;
+					case "": // in case if it happens
+						log.info("user <{}> missed a command", message.getUsername());
+						message.setContents("a command is required");
+						writer.write(mapper.writeValueAsString(message));
+						writer.flush();
+						break;
+					case "@":
+						log.info("user <{}> whispered to nobody message: <{}>", user, message.getContents());
+						message.setContents("No username is provided. The proper usage is <@username> <message>");
+						writer.write(mapper.writeValueAsString(message));
+						writer.flush();
+						break;
+					default:
+						if (command.charAt(0) != '@') { // Unknown command:
+							if (previousCommand != "") {
+								message.setCommand(previousCommand);
+								message.setContents(command + " " + message.getContents());
+								command = previousCommand;
+								repeatCommand = true;
+								break;
+							}
+							log.info("user <{}> gave non-recognised command <{}>", user, command);
+							message.setContents(
+									df.format(new Date()) + ": Server doesn't recognise command <" + command + ">.");
+							writer.write(mapper.writeValueAsString(message));
+							writer.flush();
 							break;
 						}
-						log.info("Java doesn't recognize the command: <{}>", command);
+						// Whispering:
+						String user2 = command.substring(1);
+						previousCommand = command;
+						if (!connectedUsers.containsKey(user2)) {
+							log.info("user <{}> whispered to non-connected user <{}> message <{}>", user, user2,
+									message.getContents());
+							message.setContents(df.format(new Date()) + ": user <" + user2 + "> is not connected");
+							writer.write(mapper.writeValueAsString(message));
+							writer.flush();
+							break;
+						}
+						if (user2 == user) {
+							log.info("user <{}> whispered to himself message: <{}>", user, message.getContents());
+						} else {
+							log.info("user <{}> whispered to user <{}> message: <{}>", message.getUsername(), user2,
+									message.getContents());
+						}
+						message.setContents(
+								df.format(new Date()) + " <" + user + "> (whisper): " + message.getContents()); // ugly
+						// Write to the other user:
+						PrintWriter writerOther = new PrintWriter(connectedUsers.get(user2).getOutputStream());
+						writerOther.write(mapper.writeValueAsString(message));
+						writerOther.flush();
 						break;
-				}
-			}
-
+					} // switch end
+				} // while repeat command end
+			} // while end
 		} catch (IOException e) {
 		//	users.remove(message.getUsername());					//remove user from the list
 			log.error("Something went wrong :/", e);
